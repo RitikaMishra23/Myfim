69f356dbfccb86dfbc745ad60979f835c49064e8
Ritika Mishra <103439948+RitikaMishra23@users.noreply.github.com>
2023-02-07 20:16:34 +0530

    Delete keys.py

--- myFimBOT/source/modules/keys.py
+++
@@ -1,5 +0,0 @@
-keys={
-    'speech_key':"43b95c4f337c490b9c133104c3410aa1",'speech_region':"eastus",'vision_key':"8b2cf393996743128129ae17e66cba88",
-    'vision_endpoint':"https://ritika0523.cognitiveservices.azure.com/",'intent_app_id':"a30bfefb-4e04-42ab-b95b-33bdbe42b4d8",
-    'intent_key':"770e41346e5b4edcb4312f4214858ead",'intent_endpoint':"https://ritika823.cognitiveservices.azure.com/"
-}
